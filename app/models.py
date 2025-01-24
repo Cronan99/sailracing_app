d47a5a09f472ced157e953677b799fbe1b54c803
Cronan99 <albincronmark@gmail.com>
2025-01-24 16:58:40 +0100

    register functional and saves user to db

--- app/models.py
+++ app/models.py
@@ -3,18 +3,24 @@ from sqlalchemy import ForeignKey
 
 
 class User(db.Model):
+    """ User class contain id name and password aswell as a boolean if the user in admin or not """
+    
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
     admin = db.Column(db.Boolean, nullable=False)
 
 class Boat(db.Model):
+    """ Boat class is the boat object that is in a relation to a user """
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     name = db.Column(db.String(255), nullable=False)
     type_id = db.Column(db.Integer, db.ForeignKey("boat_type.id"))
 
 class Boat_type(db.Model):
+    """ Boat_type contains data from the web with handicap and type name """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     srs = db.Column(db.Float, nullable=False)
@@ -23,11 +29,15 @@ class Boat_type(db.Model):
     srs_sh_ns = db.Column(db.Float, nullable=False) #shorthanden no spin
 
 class Race(db.Model):
+    """ Race contains id name and race date """
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     date = db.Column(db.Date, nullable=False)
 
 class Race_stat(db.Model):
+    """ Race_stat contains statistics from races in relation to a boat and a race to show its specific time """
+
     id = db.Column(db.Integer, primary_key=True)
     race_id = db.Column(db.Integer, ForeignKey("race.id"))
     boat_id = db.Column(db.Integer, ForeignKey("boat.id"))
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,54 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
 from app.models import *
 from flask import current_app as app
+from time import sleep
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
     if True:
         boat_types = Boat_type.query.all()
-
+        
         return render_template("index.html", boat_types=boat_types)
     else:
         return render_template("login.html")
+    
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Gets username and password from the loginpage and then saves the user to the database."""
+    
+    if request.method=="POST":
+
+        username = request.form.get("Username")
+        password = request.form.get("Password")
+
+        # Checks that both username and psasword field are filled in
+        if not username or not password:
+            flash("Username and Password are required for registration!")
+            return redirect(url_for("register"))
+        
+        # Checks for already existing users with that name
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username is already in use! Please choose another.")
+            return redirect(url_for("register"))
+        
+        # Generates hash, creates user object and saves it to the database
+        password_hash= generate_password_hash(password)
+
+        user = User(username=username, password_hash=password_hash, admin=False)
+
+        db.session.add(user)
+        db.session.commit()
+
+        # After the user is created its redirected to the login page
+        flash("Registration is successful! Please login.")
+        return redirect(url_for("login"))
+        
+
+    return render_template("register.html")
