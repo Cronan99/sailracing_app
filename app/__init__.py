f160d29f6726928723f96c735c23186f0f784946
Cronan99 <albincronmark@gmail.com>
2025-01-28 14:43:29 +0100

    login added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    load_dotenv()
+    app.secret_key = os.getenv("SECRET_KEY")
 
     db.init_app(app)
     
